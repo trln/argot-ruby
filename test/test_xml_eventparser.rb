641d7555fe63d296273e3af845bdfb1289341274
Adam Constabaris <adam_constabaris@ncsu.edu>
2016-11-17 16:20:10 -0500

    Update version to 0.0.3, add test for XML Event Parser

--- test/test_xml_eventparser.rb
+++ test/test_xml_eventparser.rb
@@ -1,18 +1,24 @@
+require_relative './util'
 require 'minitest/autorun'
 require 'argot/xml'
 
 class EventParserTest < Minitest::Test
 
         def setup
-            @instance = Argot::XML::EventParser.new("USMARC")
+            @instance = Argot::XML::ICEExtractor.new(Util.get_file("ice-data.xml"))
         end
 
-        def test_instantiate
-            refute_nil @instance
-        end
 
         def test_default_procesor 
-            flunk "Not written yet"
+          recs = []
+          @instance.each { |x| recs << x }
+
+          assert_equal 1, recs.length, "Should have one record in the test file"
+          assert(recs[0].has_key?(:chapters), "Should be a :chapters key in result")
+          assert_equal(3, recs[0][:chapters].length, "Should be three chapters")
+          assert_equal("Chapter One", recs[0][:chapters][0][:title], "Unexpected chapter title in first position")
+
+
         end
 
 end
