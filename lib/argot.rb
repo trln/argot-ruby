e647f58d80fdc6af4605c2bdac541520190f2241
Adam Constabaris <adjam@noreply.github.com>
2017-01-30 07:47:40 -0500

    autorequire validator

--- lib/argot.rb
+++ lib/argot.rb
@@ -1,11 +1,3 @@
-##
-# Argot -- tools for working with TRLN shared record format.
-# +require+-ing this module also imports the +Argot::Validator+ and +Argot::Reader+ classes.
-# 
-# Other tools and utilities that are available
-# {+Argot::TrajectJSONWriter+}[rdoc-ref:Argot::TrajectJSONWriter] (+require 'argot/traject_writer'+) - a Traject Writer implementation that converts the usual Traject output format more friendly 
-require 'argot/validator'
-
 if RUBY_PLATFORM =~ /java/
     require 'argot/jruby/reader'
 else
@@ -14,3 +6,7 @@ end
 require 'argot/pipeline'
 require 'argot/flattener'
 require 'argot/suffixer'
+require 'argot/validator'
+module Argot
+    
+end
