a480d579effcee549b333726ccfc6890e608ebf2
Adam Constabaris <adam_constabaris@ncsu.edu>
2017-06-06 09:55:29 -0400

    Add get_json method to simplify reading test JSON files in

--- test/util.rb
+++ test/util.rb
@@ -1,25 +1,31 @@
 module Util
-    TEST_FILES = File.expand_path("../data", __FILE__)
+  TEST_FILES = File.expand_path('../data', __FILE__)
 
-    def self.find_file(name) 
-        path = File.join(Util::TEST_FILES, name)
-        if not File.exist?(path)
-            raise "Unable to find required test file #{name}"
-        end
-        yield path if block_given?
-        path
-    end
+  def self.find_file(name)
+    path = File.join(Util::TEST_FILES, name)
+    raise "Can't find #{name}" unless File.exist?(path)
+    yield path if block_given?
+    path
+  end
 
-    def self.get_file(name)
-        file = File.open(find_file(name), 'r')
-        if block_given?
-            begin
-                yield file
-            ensure
-                file.close
-            end
-        end
-        file
+  def self.get_file(name)
+    file = File.open(find_file(name), 'r')
+    if block_given?
+      begin
+        yield file
+      ensure
+        file.close
+      end
     end
+    file
+  end
+
+  def self.get_json(name)
+    r = {}
+    get_file(name) do |f|
+      r = JSON.parse(f.read)
+      yield r if block_given?
+    end
+    r
+  end
 end
-
