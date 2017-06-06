e70806a9aab7c6c4ba72fbbaa715c1c4981c3412
Adam Constabaris <adam_constabaris@ncsu.edu>
2017-06-06 09:57:00 -0400

    rubocop compliance

--- lib/argot.rb
+++ lib/argot.rb
@@ -1,12 +1,16 @@
+require 'argot/meta'
 if RUBY_PLATFORM =~ /java/
-    require 'argot/jruby/reader'
+  require 'argot/jruby/reader'
 else
-    require 'argot/mri/reader'
+  require 'argot/mri/reader'
 end
 require 'argot/pipeline'
 require 'argot/flattener'
 require 'argot/suffixer'
 require 'argot/validator'
+
+# Argot is TRLN's shared ingest format; it is a dialect of JSON, and
+# represents (primarily) a tranformation of MARC into a more human-readable
+# format close to what might be ingested directly into Solr.
 module Argot
-    
 end
