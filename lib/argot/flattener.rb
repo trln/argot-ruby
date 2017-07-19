0c59facbecee377483801dd1677ecdb7a29a36a6
Luke Aeschleman <lukeaeschleman@gmail.com>
2017-07-19 17:46:14 -0400

    flattener can create a json blob

--- lib/argot/flattener.rb
+++ lib/argot/flattener.rb
@@ -1,57 +1,65 @@
 ##
 # Flattens nested Argot JSON
-class Argot::Flattener
+module Argot
 
-	def self.combine(hash1, hash2)
-		hash2.each do |k,v|
-			if hash1.has_key?(k)
-				hash1[k] = Array(hash1[k])
-				hash1[k] = hash1[k] + v
-			else
-				hash1[k] = v
-			end
-		end
-		hash1
-	end
+  # Flattens an argot hash
+  class Flattener
 
-	def self.flatten(value,parent,ignore = nil)
-		flattened = Hash.new
-	
-		if value.is_a?(Hash)
-			value.each do |k,v|
-				expanding_key = "#{parent}_#{k}"
-				flattened = combine(flattened, flatten(v,expanding_key))
-			end
-		elsif value.is_a?(Array) and value[0].is_a?(Hash)
-			value.each do |v|
-				flattened = combine(flattened, flatten(v,parent))
-			end
-		else
-			unless value.nil?
-				if flattened[parent].nil?
-					flattened[parent] = Array.new
-				end
-				flattened[parent] << value
-			end
-		end
-		flattened
-	end
+    JSON_BLOB_FIELDS = %w[items].freeze
 
-	def self.process(input)
-		flattened = Hash.new
-	
-		
-		input.each do |k,v|
-			unless v.nil?
-  			flattened = combine(flattened,flatten(v,k))
-			end
-		end
+    def self.combine(hash1, hash2)
+      hash2.each do |k, v|
+        if hash1.key?(k)
+          hash1[k] = Array(hash1[k])
+          hash1[k] = hash1[k] + v
+        else
+          hash1[k] = v
+        end
+      end
+      hash1
+    end
 
-  	flattened.each do |k,v|
-			if v.length == 1
-				flattened[k] = v[0]
-			end
-		end
-	end
-	
+    def self.flatten(value, parent, ignore = nil)
+      flattened = {}
+
+      if value.is_a?(Hash)
+        value.each do |k,v|
+          expanding_key = "#{parent}_#{k}"
+          flattened = combine(flattened, flatten(v, expanding_key))
+        end
+      elsif value.is_a?(Array) and value[0].is_a?(Hash)
+        value.each do |v|
+          flattened = combine(flattened, flatten(v, parent))
+        end
+      else
+        unless value.nil?
+          flattened[parent] = [] if flattened[parent].nil?
+          flattened[parent] << value
+        end
+      end
+      flattened
+    end
+
+    def self.create_json_blob(value)
+      if value.is_a?(Array)
+        value.map!(&:to_s)
+      else
+        value.to_s
+      end
+    end
+
+    def self.process(input)
+      flattened = {}
+
+      input.each do |k, v|
+        v = create_json_blob(v) if JSON_BLOB_FIELDS.include?(k)
+        flattened = combine(flattened, flatten(v, k)) unless v.nil?
+      end
+
+      flattened.each do |k, v|
+        flattened[k] = v[0] if v.length == 1
+      end
+    end
+  end
+
 end
